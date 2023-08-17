1f6e42675c78a61f2e48d86bc59e99c88f50dbdb
Andy Patari <andy.patari@gmail.com>
2023-08-17 14:09:09 -0400

    filter functions

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,6 +5,7 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -14,12 +15,17 @@ function ShoppingList({ items }) {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
-
+  }).filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()));
+  //filter the above again once I have the text filter controlled
+  
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        searchText={searchText}
+        onSearchChange={setSearchText}
+        onCategoryChange={handleCategoryChange} 
+        />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
